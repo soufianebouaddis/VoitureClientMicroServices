f38e3b16501e055ef5d343e969753663c9768764
mjustatool <fousenaibdisbdis@gmail.com>
2023-12-23 12:20:40 +0100

    MicroService with consul

--- RegisterService/src/main/java/com/vc/RegisterService/RegisterServiceApplication.java
+++ RegisterService/src/main/java/com/vc/RegisterService/RegisterServiceApplication.java
@@ -2,10 +2,10 @@ package com.vc.RegisterService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class RegisterServiceApplication {
 
 	public static void main(String[] args) {
